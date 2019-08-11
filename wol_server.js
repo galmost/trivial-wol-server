c830c4468139c4c0e7c2b7c2dbba50ca48d653bc
Galmost <gologan.alexandru@gmail.com>
2019-08-11 15:28:26 +0300

    Initial dummy release

---
+++ wol_server.js
@@ -0,0 +1,52 @@
+const wol = require('wol');
+var http = require('http');
+var url = require('url');
+var os = require('os');
+
+var getIpAddress = function() {
+    var ifaces = os.networkInterfaces();
+    var ips = {};
+    for (var dev in ifaces) {
+        var alias=0;
+        ifaces[dev].forEach(function(details){
+            if (details.family=='IPv4') {
+                // console.log(dev+(alias?':'+alias:''),details.address);
+                ips[dev+(alias?':'+alias:'')] = details.address;
+                ++alias;
+            }
+        });
+    }
+    return ips;
+};
+
+
+// All present interfaces 
+var server_ip = getIpAddress();
+console.log( 'Server available interfaces:' + '\n', server_ip);
+
+// Using wifi1, but this can be changed
+var listening_on = server_ip.wifi1
+var using_port = 1337
+
+http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  var query = url.parse(req.url, true).query;
+          var mac = query.mac;
+
+	  if (mac){
+	  	wol.wake(mac, function(err, res){
+  			console.log(query, res);
+	  	});
+
+          	res.write('Magic packet sent to: ' + mac);
+          }
+	  else {
+	        res.write('What can i help you with, sir?')
+	  }
+	  res.end();
+}).listen(using_port, listening_on);
+console.log('Server running at http://' + listening_on +':' + using_port + '/');
+
+
+
+
