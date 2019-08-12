b1cee3c2ab44fe1767b2f364dbd9134fd4ae5902
Galmost <gologan.alexandru@gmail.com>
2019-08-12 23:54:51 +0300

    Fixing indenting

--- trivial_wol_d.js
+++ trivial_wol_d.js
@@ -4,19 +4,23 @@ var url = require('url');
 var os = require('os');
 
 var getIpAddress = function() {
-    var ifaces = os.networkInterfaces();
-    var ips = {};
-    for (var dev in ifaces) {
-        var alias=0;
-        ifaces[dev].forEach(function(details){
-            if (details.family=='IPv4') {
-                // console.log(dev+(alias?':'+alias:''),details.address);
-                ips[dev+(alias?':'+alias:'')] = details.address;
-                ++alias;
-            }
-        });
-    }
-    return ips;
+  var ifaces = os.networkInterfaces();
+  var ips = {};
+
+  for (var dev in ifaces) {
+    var alias=0;
+
+    ifaces[dev].forEach(function(details){
+
+      if (details.family=='IPv4') {
+        // console.log(dev+(alias?':'+alias:''),details.address);
+        ips[dev+(alias?':'+alias:'')] = details.address;
+        ++alias;
+      }
+
+    });
+  }
+  return ips;
 };
 
 
@@ -24,31 +28,30 @@ var getIpAddress = function() {
 var server_ip = getIpAddress();
 console.log( 'Server available interfaces:' + '\n', server_ip);
 
-// Using eth0, but this can be changed based on interfaces available
+// Local variant using eth0, but this can be changed based on interfaces available
 // var listening_on = server_ip.eth0
 
-// Container variant is essentially allow all traffic to be picked up 
+// Container ip variant is essentially allow all traffic to be picked up 
 var listening_on = "0.0.0.0"
-
 var using_port = 30000
 
 http.createServer(function (req, res) {
-	  res.writeHead(200, {'Content-Type': 'text/plain'});
-	  var query = url.parse(req.url, true).query;
-          var mac = query.mac;
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var query = url.parse(req.url, true).query;
+  var mac = query.mac;
 
-	  if (mac){
-	  	wol.wake(mac, function(err, res){
-  			console.log(query, res);
-	  	});
+  if (mac){
+    wol.wake(mac, function(err, res){
+	    console.log(query, res);
+    });
 
-          	res.write('Magic packet sent to: ' + mac);
-          }
-	  else {
-	        res.write('What can i help you with, sir?')
-	  }
-	  res.end();
+    res.write('Magic packet sent to: ' + mac);
+  } 
+  else { res.write('What can i help you with, sir?') }
+  res.end();
+
 }).listen(using_port, listening_on);
+
 console.log('Server running at http://' + listening_on +':' + using_port + '/');
 
 
